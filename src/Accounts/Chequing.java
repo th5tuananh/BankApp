59012cfe8cb22c383e11857721e7a21b5a856ddb
frostiee22 <mkallo42@gmail.com>
2015-02-07 23:31:23 -0400

    fix spelling

--- src/Accounts/Chequing.java
+++ src/Accounts/Chequing.java
@@ -3,11 +3,11 @@ package Accounts;
 /**
  * Created by Mitra on 1/27/2015.
  */
-public class Checking extends Account {
+public class Chequing extends Account {
 
     private int numCheques;
 
-    public Checking (int number, double balance, int cheques){
+    public Chequing(int number, double balance, int cheques){
         super(number , balance);
         numCheques = cheques;
     }
